f9c2d6ecd6391a52136a861e518c8b0e1d0c792d
vortexmfr <121253776+vortexmfr@users.noreply.github.com>
2023-01-26 18:11:02 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello world!");
+=======
+        System.out.println("Hola Mundo!");
+>>>>>>> f590add (Initial commit)
     }
 }
