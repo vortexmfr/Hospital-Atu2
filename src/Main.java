8208066395904abd952545958427ee8ca135a581
vortexmfr <121253776+vortexmfr@users.noreply.github.com>
2023-01-26 19:33:22 +0100

    AsASs

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello world!");
-=======
+
         System.out.println("Hola Mundo!");
->>>>>>> f590add (Initial commit)
     }
 }
